af6b29b1aa7bdf85df32e666f22eabd27754af52
Melodie Joseph <josmelodie@gmail.com>
2019-11-03 17:51:28 +1300

    Challenge: Write a function to split a string and convert it into an array of words.

---
+++ Convert_String_to_Array/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Challenge: Write a function to split a string and convert it into an array of words.
+
+namespace Convert_String_to_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(" ", Solution.StringToArray("Hello this is awesome")));
+        }
+    }
+
+    public static class Solution
+    {
+        public static string[] StringToArray(string str)
+        {
+            string[] temp = str.Split(' ');
+            return temp;
+
+            //return str.Split(' ');
+        }
+    }
+}
