8739fe805c283e5040f6c6d2790c2670c8c281db
Melodie Joseph <josmelodie@gmail.com>
2019-11-03 21:15:34 +1300

    Create a function that removes the first and last characters of a string.

---
+++ Remove_First_and_Last_Char/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Challemge: Create a function that removes the first and last characters of a string.
+namespace Remove_First_and_Last_Char
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.RemoveChar("Removing"));
+        }
+    }
+
+    public static class Kata
+    {
+        public static string RemoveChar(string s)
+        {
+            //create a temporary char array to hold the string
+            char[] temp = s.ToCharArray();
+            //convert the char array to a List
+            List<char> secondString = temp.ToList();
+
+            //Remove the first item
+            secondString.RemoveAt(0);
+            //Remove the last item
+            secondString.RemoveAt(secondString.Count - 1);
+
+            //return the List as a new string
+            return new string(secondString.ToArray());
+
+            //return s.Remove(s.Length - 1).Remove(0, 1);
+        }
+    }
+}
