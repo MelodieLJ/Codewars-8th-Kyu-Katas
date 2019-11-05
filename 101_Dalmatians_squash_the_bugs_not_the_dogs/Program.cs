a0a55680fedcc809d30bf3d3e239f3bde34a1bda
Melodie Joseph <josmelodie@gmail.com>
2019-11-05 22:09:57 +1300

    Challenge involving loops, strings and ints

---
+++ 101_Dalmatians_squash_the_bugs_not_the_dogs/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Challenge: Your friend has been out shopping for puppies. He arrives back with multiple dogs, and you simply do not know how to respond!
+//By repairing the function provided, you will find out exactly how you should respond, depending on the number of dogs he has.
+//The number of dogs will always be a number and there will always be at least 1 dog.
+
+namespace _101_Dalmatians_squash_the_bugs_not_the_dogs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.HowManyDalmatians(50));
+        }
+    }
+
+    public static class Kata
+    {
+        public static string HowManyDalmatians(int n)
+        {
+            if (n <= 10) return "Hardly any";
+            if (n <= 50) return "More than a handful!";
+            if (n == 101) return "101 DALMATIONS!!!";
+            return "Woah that's a lot of dogs!";
+        }
+    }
+}
---
+++ 101_Dalmatians_squash_the_bugs_not_the_dogs/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("101_Dalmatians_squash_the_bugs_not_the_dogs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("101_Dalmatians_squash_the_bugs_not_the_dogs")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4cd4c2d6-9afc-4360-94a1-05a9bf11cf73")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
