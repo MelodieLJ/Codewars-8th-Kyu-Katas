1175c63efa6cbae8b3e55ff4c3e175481269dfab
Melodie Joseph <josmelodie@gmail.com>
2019-11-10 16:33:03 +1300

    Challenge: Return the nth Even Number. E.g. NthEven(3) //=> 4, the 3rd even number is 4 (0, 2, 4)

---
+++ Return_nth_Even_Number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Challenge: Return the nth Even Number. E.g. NthEven(3) //=> 4, the 3rd even number is 4 (0, 2, 4)
+namespace Return_nth_Even_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Output to be 2597466
+            Console.WriteLine(Kata.NthEven(1298734));
+        }
+    }
+
+    public static class Kata
+    {
+        public static int NthEven(int n)
+        {
+            return (n * 2) - 2;
+        }
+    }
+}
