3b3ea82c410eef6ffd7d2951afcae1c574ac503d
Melodie Joseph <josmelodie@gmail.com>
2019-11-10 16:52:48 +1300

    Given the number (n), populate an array with all numbers up to and including that number, but excluding zero.

---
+++ Counting_with_Array/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Challenge: Given the number (n), populate an array with all numbers up to and including that number, but excluding zero.
+
+namespace Counting_with_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(",", Kata.MonkeyCount(10)));
+        }
+    }
+
+    public static class Kata
+    {
+        public static int[] MonkeyCount(int n)
+        {
+            List<int> temp = new List<int>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                temp.Add(i);
+            }
+
+            return temp.ToArray();
+        }
+    }
+}
