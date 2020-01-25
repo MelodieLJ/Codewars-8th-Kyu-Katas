b315d31229d2bcc9d7b46e6a6ca536335295672f
Melodie Joseph <josmelodie@gmail.com>
2020-01-25 19:00:02 +1300

    Challenge: Create a function that checks if a number n is divisible by two numbers x AND y.

---
+++ Check if n is divisible by x and y/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Check_if_n_is_divisible_by_x_and_y
+{
+    //Challenge: Create a function that checks if a number n is divisible by two numbers x AND y.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.isDivisible(12,3,4));
+        }
+    }
+
+    public class Kata
+    {
+        public static bool isDivisible(long n, long x, long y)
+        {
+            if (n % x == 0 && n % y == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
